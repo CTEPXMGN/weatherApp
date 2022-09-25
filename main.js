9d5b73b0c23906f13bf3a765a58652c5882fe7b0
CTEPXMGN <fuser-mgn@ya.ru>
2022-09-25 16:54:36 +0500

    switch tabs js

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 import { UI_ELEMENTS } from "./view.js";
 
+const tabs = document.querySelectorAll('.tab');
+
+// Переключение табов
+for (const item of tabs) {
+    item.addEventListener('click', switchTab);
+}
+
+function switchTab() {
+    for (const item of tabs) {
+        item.classList.remove('active-tab');
+    };
+    this.classList.add('active-tab');
+};
+
 UI_ELEMENTS.FIND_CITY.addEventListener('click', function(event) {
     event.preventDefault();
-
-    console.log('!!!');
 });
